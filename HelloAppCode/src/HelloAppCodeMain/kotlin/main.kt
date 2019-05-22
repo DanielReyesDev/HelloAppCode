1a66133ecc4a223df792a2b3bcd0462651d82773
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-21 22:01:23 -0500

    Initial project

---
+++ HelloAppCode/src/HelloAppCodeMain/kotlin/main.kt
@@ -0,0 +1,16 @@
+//
+//  main.kt
+//  HelloAppCode
+//
+//  Created by Daniel Reyes Sánchez on 2019-05-21.
+//  Copyright © 2019 Daniel Reyes Sánchez. All rights reserved.
+//
+
+import kotlinx.cinterop.*
+import platform.Foundation.*
+
+fun main(args: Array<String>) {
+    autoreleasepool {
+        NSLog("Hello, World!")
+    }
+}
